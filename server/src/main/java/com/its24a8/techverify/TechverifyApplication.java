a08078932a22afa355c537242c7bda60f8bda691
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2024-05-26 09:54:32 +0900

    add Next.js project

---
+++ server/src/main/java/com/its24a8/techverify/TechverifyApplication.java
@@ -0,0 +1,13 @@
+package com.its24a8.techverify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechverifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechverifyApplication.class, args);
+	}
+
+}
